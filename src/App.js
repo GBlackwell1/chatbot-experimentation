d248150523d26c92fd1c4f87954d10dcac38d834
Gabriel Blackwell <76234706+GBlackwell1@users.noreply.github.com>
2022-08-29 20:45:52 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from "react";
+import FormChatbot from "./Chatbot/FormChatbot";
+//import PropTypes from "prop-types";
+import "./App.css";
 
+function App() {
+  return (
+    <FormChatbot />
+  );
+}
+export default App;
